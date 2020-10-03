ceca7e376e9962c388dff9f05536865446e3d9a0
Guido Barbaglia <guido@vinomofo.com>
2020-10-03 19:57:36 +1000

    Logs

--- api/hello/main.go
+++ api/hello/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Kalimaha/hexa-go/pkg/repositories"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	fmt.Printf("<== == == == == == ==>\n")
+	fmt.Printf("HTTP Method: %v\n", request.HTTPMethod)
+	fmt.Printf("Headers: %v\n", request.Headers)
+	fmt.Printf("Body: %v\n", request.Body)
+	fmt.Printf("<== == == == == == ==>\n")
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       repositories.Ciao(),
